fb668413babee88349bbb3d94cffe97deb317b5a
unknown <sivaprakashui@gmail.com>
2021-12-27 22:12:08 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Main from './components/Main';
+
+const App = () => {
+  return (
+    <div className="container">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
